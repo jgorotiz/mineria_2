6fa6e6241dd6c632c8c73f9f4b9ca0c46a451f8f
jgorotiz <jgorotiz@espol.edu.ec>
2018-12-04 03:35:28 -0500

    Tema1

--- script.R
+++ script.R
@@ -3,54 +3,51 @@ library(e1071)
 library(forecast)
 setwd("C:\\Users\\JONATHAN\\Desktop\\2018-2T\\mineria")
 #Load data
-train = read.csv("cancer_train.csv")
-test = read.csv("cancer_test.csv")
-View(train)
-View(test)
+train_data = read.csv("cancer_train.csv")
+test_data = read.csv("cancer_test.csv")
+View(train_data)
+View(test_data)
 
 #Data treatment
-train$radio <- scale(train$radio)
-train$simetria <- scale(train$simetria)
-test$radio <- scale(test$radio)
-test$simetria <- scale(test$simetria)
-train$tipo <- sapply(train$tipo,switch,"M" = 1, "B" = 0)
-test$tipo <- sapply(test$tipo,switch,"M" = 1, "B" = 0)
+train_data$radio <- scale(train_data$radio)
+train_data$simetria <- scale(train_data$simetria)
+test_data$radio <- scale(test_data$radio)
+test_data$simetria <- scale(test_data$simetria)
+train_data$tipo <- sapply(train_data$tipo,switch,"M" = 1, "B" = 0)
+test_data$tipo <- sapply(test_data$tipo,switch,"M" = 1, "B" = 0)
 
 #Logistic regression
-mylogit <- glm(tipo ~ radio + simetria, data = train, family = "binomial")
+mylogit <- glm(tipo ~ radio + simetria, data = train_data, family = "binomial")
 summary(mylogit)
 
 #Prediction
-train$prediction <- predict(mylogit, newdata = train, type = "response" )
-test$prediction  <- predict(mylogit, newdata = test , type = "response" )
+train_data$prediction <- predict(mylogit, newdata = train_data, type = "response" )
+test_data$prediction  <- predict(mylogit, newdata = test_data , type = "response" )
 
-#Apply cutoffs
-test$cutoff.1 <- ifelse(test$prediction>0.1, 1, 0)
-test$cutoff.5 <- ifelse(test$prediction>0.5, 1, 0)
-test$cutoff.9 <- ifelse(test$prediction>0.9, 1, 0)
+test_data$cutoff.1 <- ifelse(test_data$prediction>0.1, 1, 0)
+test_data$cutoff.5 <- ifelse(test_data$prediction>0.5, 1, 0)
+test_data$cutoff.9 <- ifelse(test_data$prediction>0.9, 1, 0)
 
 #Confusion matrix
-#Cutoff 0.1
-con.mat <- confusionMatrix(factor(test$tipo),factor(test$cutoff.1))
+con.mat <- confusionMatrix(factor(test_data$tipo),factor(test_data$cutoff.1))
 con.mat$table
 str(con.mat)
 con.mat$overall
 con.mat$byClass
 
-#Cutoff 0.5
-con.mat <- confusionMatrix(factor(test$tipo),factor(test$cutoff.5))
+con.mat <- confusionMatrix(factor(test_data$tipo),factor(test_data$cutoff.5))
 con.mat$table
 str(con.mat)
 con.mat$overall
 con.mat$byClass
 
-#Cutoff 0.9
-con.mat <- confusionMatrix(factor(test$tipo),factor(test$cutoff.9))
+con.mat <- confusionMatrix(factor(test_data$tipo),factor(test_data$cutoff.9))
 con.mat$table
 str(con.mat)
 con.mat$overall
 con.mat$byClass
 
-#Train and test errors
-accuracy(train$prediction,train$tipo)
-accuracy(test$prediction,test$tipo)
+
+
+accuracy(train_data$prediction,train_data$tipo)
+accuracy(test_data$prediction,test_data$tipo)
